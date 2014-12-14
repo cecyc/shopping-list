9c12d15ff3007d5f73f0762b9b2745b92cf10480
cecyc <cecycorrea@gmail.com>
2014-12-13 19:25:24 -0600

    Fixing Jquery and styling, added icon

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
-$(document).ready(function() {
+function addListItem() {
+  var newItem = $('#newItem');
+  var add = newItem.val();
+  var itemList = $('#itemList')
+  var singleItem = $('<li><button class="check">X</button>' + add + '</li>');
 
-$("input").val();
+  itemList.append(singleItem);
+  $(newItem).val('');
 
+}
+
+function strikeItem() {
+    $('li').toggleClass('strikethrough');
+}
+
+$(function() {
+  var button = $('#addItem');
+  var newItem = $('#newItem');
+  var list = $('#itemList');
+  button.on('click', addListItem);
+  button.on('keypress', addListItem);
+  list.on('click', '.check', strikeItem);
+  newItem.on('keydown', function(e) {
+        if (e.which == 13) {
+            addListItem();
+        }
+    });
 });
